1c7bc581588b3a1fc40c4f2b3239d869ddcd3c23
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-06-28 19:09:22 +0200

    Revert which-key opacity

--- lua/mngrm3a/utils.lua
+++ lua/mngrm3a/utils.lua
@@ -48,7 +48,7 @@ local wichkey_options = {
         position = "bottom",      -- bottom, top
         margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
         padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 80,            -- value between 0-100 0 for fully opaque and 100 for fully transparent
+        winblend = 0,             -- value between 0-100 0 for fully opaque and 100 for fully transparent
         zindex = 1000,            -- positive value to position WhichKey above other floating windows.
     },
     layout = {
