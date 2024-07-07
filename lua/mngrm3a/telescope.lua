0c0e12dc05ff8cbccfead903d6a40c7ca6577f22
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-07-07 17:26:44 +0200

    Change telescope layout to vertical

--- lua/mngrm3a/telescope.lua
+++ lua/mngrm3a/telescope.lua
@@ -1,10 +1,16 @@
+local telescope_options = {
+    defaults = {
+        layout_strategy = 'vertical',
+        layout_config = { width = 0.95, height = 0.95 },
+    },
+}
 -- -----------------------------------------------------------------------------
 -- section: setup
 -- -----------------------------------------------------------------------------
 local M = {}
 
 function M.setup()
-    require("telescope").setup()
+    require("telescope").setup(telescope_options)
     require('telescope').load_extension("fzf")
 end
 
